3aa251d6df73ae8af15a4ab67678c1e09beaf29a
Noe Cruz | Zurckz <contactozurckz@gmail.com>
2020-07-21 00:01:31 -0500

    :zap: restructured project

--- aplicacion/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import SubmitField
-from flask_wtf.file import FileField, FileRequired
-
-class UploadForm(FlaskForm):
-    photo = FileField('selection imagen:',validators=[FileRequired()])
-    submit = SubmitField('Submit')
-    
