61f48bda1cbbb1ee837ced460ff919f9541fe32e
Noe Cruz | Zurckz <contactozurckz@gmail.com>
2020-08-05 09:56:55 -0500

    :zap: ContactForm implemented

--- app/app.py
+++ app/app.py
@@ -1,6 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_mail import Mail
+from flask_mail import Message
 
 app = Flask(__name__)
+app.config['MAIL_SERVER']='http://mail.mailkitchen.com/'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'uriel_monsivais@hotmial.com'
+app.config['MAIL_PASSWORD'] = 'monsivaismireles'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)
 
 
 @app.route('/')
@@ -14,3 +23,17 @@ def signin():
 @app.route('/welcome')
 def welcome():
     return render_template('welcome.html')
+
+@app.route('/contact', methods=['POST'])
+def contact():
+    request_c = request.form
+    try:
+        msg = Message("New request",
+                  sender="contacto.sasit.red",
+                  recipients=[request_c['mail']])        
+        msg.body = "Nuevo correo: "+request_c['mail']
+        mail.send(msg)
+    except ValueError:
+        print("Email error")
+        print(ValueError)
+    return render_template('index.html')
